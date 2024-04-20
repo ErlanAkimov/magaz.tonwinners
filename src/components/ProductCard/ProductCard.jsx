5f134cc07d9723db275d2b82f0deda99564eecd8
erlan_akimov <erlan_akimov_dev@mail.ru>
2024-04-21 04:59:37 +0500

    got to sleep

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,9 @@
+import styles from './prodCard.module.scss'
+
+export const ProductCard = ({ data }) => {
+	return (
+		<div className={styles.wrapper}>
+			
+		</div>
+	)
+}
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,73 @@
-import styles from './homepage.module.scss'
+import styles from './homepage.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import mac from '../../assets/images/mac.png';
+
+import macMini from '../../assets/images/mac mini.png';
+import coins from '../../assets/images/coin mini.png';
+import pods from '../../assets/images/pods.png';
+import phones from '../../assets/images/iphones.png';
+import sneak from '../../assets/images/sneakers.png';
+import { ProductCard } from '../../components/ProductCard/ProductCard';
+
+const cats = [
+	{
+		img: macMini,
+		text: 'macbooks',
+	},
+	{
+		img: coins,
+		text: 'dropcoins',
+	},
+	{
+		img: pods,
+		text: 'headphones',
+	},
+	{
+		img: phones,
+		text: 'iphones',
+	},
+	{
+		img: sneak,
+		text: 'sneakers',
+	},
+];
+
 function Homepage() {
 	return (
 		<div className={styles.wrapper}>
-			
+			<Swiper className={styles.banner} spaceBetween={30}>
+				<SwiperSlide className={styles.bannerItem}>
+					<h2 className={styles.bannerTitle}>
+						MacBook Pro <br /> Giveaway
+					</h2>
+					<p className={styles.bannerDesc}>
+						- Make purchases from <br /> 15 TON and participate <br />
+						in the draw
+					</p>
+					<img src={mac} alt="" />
+				</SwiperSlide>
+			</Swiper>
+
+			<div className={styles.categories}>
+				{cats.map((cat) => {
+					return (
+						<div className={styles.catItems}>
+							<div className={styles.catImageBlock}>
+								<img src={cat.img} alt="" />
+							</div>
+							<p className={styles.catText}>{cat.text}</p>
+						</div>
+					);
+				})}
+			</div>
+
+			<div className={styles.catalog}>
+				<h2 className={styles.catalogTitle}>Neww from MAGAZ</h2>
+				<ProductCard data={{
+					name: 'dropcoin'
+				}} />
+			</div>
 		</div>
-	)
+	);
 }
 export default Homepage;
