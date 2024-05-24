8b89d37dabafbca5ad1414cbc264951dc69e3b15
erlan_akimov <erlan_akimov_dev@mail.ru>
2024-05-24 23:53:18 +0500

    fix + refactory

--- src/pages/Productpage/components/otherproducts/OtherProducts.jsx
+++ src/pages/Productpage/components/otherproducts/OtherProducts.jsx
@@ -1,23 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
 import styles from './other.module.scss';
 
 import { ProductCard } from '/src/components/ProductCard/ProductCard';
-import { api_server } from '../../../../main';
-import axios from 'axios';
 
-export const OtherProducts = () => {
-  const { productId } = useParams();
-  const [products, setProducts] = useState(null)
-  
-
-  useEffect(() => {
-    axios.get(`${api_server}/api/get-other-products?id=${productId}`).then(res => setProducts(res.data))
-  }, [])
-  
+export const OtherProducts = ({ products }) => {  
 	return (
 		<div className={styles.productLine}>
 			<h4 className={styles.prodTitle}>Other products</h4>
