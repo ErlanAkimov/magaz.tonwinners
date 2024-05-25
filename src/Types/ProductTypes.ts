6df4073a0eda035c8dd6814e83978ff9ccaaeea4
erlan_akimov <erlan_akimov_dev@mail.ru>
2024-05-25 05:42:25 +0500

    interfaces update

--- src/Types/ProductTypes.ts
+++ src/Types/ProductTypes.ts
@@ -1,36 +1,37 @@
 export interface ProductInterface {
-	// personal info
-	_id: string; // ObjectId()
-	product_id: number; // Уникальный номер продукта
-	name: string; // Полное название
-	description: string; // Детальное описание
-	brand: string; // Брэндинг
-	category: string[]; // Категория товара
-
-	// data structures
-	attributes: { [key: string]: string }; // Характеристики
-	variations: variationInterface[]; // Вариативные товары
-
-	// seller info
-	seller: string; // Название магазина (регистр учитывается)
-	seller_id: number; // Внутренний номер продавца
-	seller_wallet: string; // Адрес кошелька в TON
-
-	// counters
-	likes: number; // Количество лайков
+	_id: string;
+	product_id: number;
+	name: string;
+	description: string;
+	brand: string;
+	category: string[];
+	attributes: Record<string, string>;
+	variations: ProductVariationInterface[];
+	seller: string;
+	seller_id: number;
+	seller_wallet: string;
+	likes: number;
+	comments: ProductCommentInterface[];
 }
 
-interface variationInterface {
-	name: string, // color/memory_size etc.
-	avatar_small: string, // img_url (40x40)
-	product_card_images: string[] // img_urls for card
-	images: string[],	// img_urls in Array
-	types: singleVariationInterface[] // sizes/types
+export interface ProductCommentInterface {
+	user: string;
+	date: string;
+	text: string;
+	rating: number;
 }
 
-interface singleVariationInterface {
-	name: string | number, // Размер для обуви | объем памяти для техники или иные вариативные характеристики товара
-	stock: number // Остаток на складе
-	price: number // Стоимость конкретной вариации товара
-	attributes: {[key: string]: string}[] | null // Уникальные характеристики для каждой вариации (null при отсутствии)
+export interface ProductVariationInterface {
+	name: string;
+	avatar_small: string;
+	product_card_images: string[];
+	images: string[];
+	types: ProductSingleVariationInterface[];
+}
+
+export interface ProductSingleVariationInterface {
+	name: string | number;
+	stock: number;
+	price: number;
+	attributes: Record<string, string>[] | null;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import react from '@vitejs/plugin-react';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 
@@ -11,32 +10,6 @@ export default defineConfig({
 	plugins: [
 		react(),
 		nodePolyfills(),
-		createHtmlPlugin({
-			minify: true,
-			inject: {
-				data: {
-					metrika: `
-				  <!-- Yandex.Metrika counter -->
-				  <script type="text/javascript">
-					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-					m[i].l=1*new Date();
-					for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-					k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-					
-					ym(97033870, "init", {
-					  clickmap:true,
-					  trackLinks:true,
-					  accurateTrackBounce:true,
-					  webvisor:true
-					});
-				  </script>
-				  <noscript><div><img src="https://mc.yandex.ru/watch/97033870" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-				  <!-- /Yandex.Metrika counter -->
-				`,
-				},
-			},
-		}),
 	],
 	build: {
 		rollupOptions: {
