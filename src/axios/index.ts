5e4a904eb9a7c0f6c565397751b9ba8379fa4661
erlan_akimov <erlan_akimov_dev@mail.ru>
2024-06-02 15:12:30 +0500

    Обращение к локальному x-api

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const xApi = axios.create({
     baseURL: 'https://magaz.tonwinners.com/x-api'
+    // baseURL: 'http://localhost:3220/x-api'
 })
 
 export default xApi;
